743797674c9dc730472e42407a5f83519cf4c3a2
utente <utente@LAPTOP-GGB45N4B>
2020-03-24 19:43:15 +0100

    bzdgb

--- src/it/polito/tdp/model/TestModel.java
+++ src/it/polito/tdp/model/TestModel.java
@@ -7,7 +7,7 @@ public class TestModel {
 		
 		Model model = new Model();
 		//System.out.println(model.getEsami().toString());
-		System.out.println(model.calcolaSottoinsiemeEsami(18));
+		System.out.println(model.calcolaSottoinsiemeEsami(100));
 		
 	}
 
